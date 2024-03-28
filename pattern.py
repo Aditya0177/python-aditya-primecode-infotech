ee6e974b55a42de51b7e8d1b55fb30992e8309ed
Aditya0177 <165043484+Aditya0177@users.noreply.github.com>
2024-03-28 20:44:09 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,75 @@
+for y in range(1,5+1):
+    aa=""
+    for x in (" * * * * * * * ") :
+      aa+=str(x)+" "
+    print(aa)
+
+for t in range(1,5+1):
+    bb=""
+    for r in (" * * * * * * ") :
+      bb+=str(r)+" "
+      print(bb)
+
+for pp  in range (1,6):
+    aaaaabb=""
+    for xxy in (" * * * * * * ",):
+        aaaaabb=aaaaabb+str(xxy)+" "
+    print(aaaaabb)
+
+        
+for xuuuuuu in range(9):
+    if (xuuuuuu==1%9):
+        print(" * ")
+    elif(xuuuuuu==2%9):
+        print(" * * ")
+
+print("______________________________________________") 
+u=int( input(" enter the number of rows"))
+o=int(input("enter the number of colums")) 
+b=""
+for i in range(1,u+1):
+    for p in range(1,o+1):
+        b=b+("* ")
+    print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("________________________________________________________")
+
+
+
+
+
+
+f=""
+for k in range(1,6):
+    f=f+("* ")
+    if (k==1 or k==5):
+        print(" * * * * * * ")
+       
+  
+    else:
+        print( " *         * ")
+
+
+
+
+
+
+
+
