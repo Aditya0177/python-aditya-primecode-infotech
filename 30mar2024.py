a37b56dd12de9501cb0e22121f2e3ba73fe8321f
Aditya0177 <165043484+Aditya0177@users.noreply.github.com>
2024-04-02 22:19:52 +0530

    Add files via upload

--- 30mar2024.py
+++ 30mar2024.py
@@ -65,8 +65,123 @@ while cw<=2:
 
 rw=1
 cw=2
-while rw<=2:
+while rw>=0:
     container=container+alllist[rw][cw]+"  "
-   
-    rw+=1
-print(container)
+    rw-=1
+
+# rw=0
+# cw=3
+# while cw<=2:
+#     container=container+alllist[rw][cw]+"  "
+#     cw-=1
+# print(container)
+
+
+print("________________________________________________________________")
+
+myylist=[["a","b","c","d"]
+        ,["e","f","g","h"]
+        ,["i","j","k","l"]
+        ,["m","n","o","p"]]
+r=0
+c=3
+bagg=""
+while c>=0:
+    bagg=bagg+myylist[r][c]+"  "
+    c-=1
+
+r=0
+c=1
+while c<=3:
+     bagg=bagg+myylist[r][c]+"  "
+     c+=1
+     r+=1
+
+r=3
+c=2
+while c>=0:
+     bagg=bagg+myylist[r][c]+"  "
+     c-=1
+
+print(bagg)
+
+print("___________________________________________________")
+
+yourrlist=[["a","b","c","d"]
+          ,["e","f","g","h"]
+          ,["i","j","k","l"]
+          ,["m","n","o","p"]]
+r=0
+c=0
+jholaa=""
+while c<=3:
+     jholaa=jholaa+yourrlist[r][c]+"  "
+     c+=1
+
+r=1
+c=2
+while r<=3:
+    jholaa=jholaa+yourrlist[r][c]+"  "
+    r+=1
+    c-=1
+
+r=3
+c=1
+while c<=3:
+    jholaa=jholaa+yourrlist[r][c]+"  "
+    c+=1
+print(jholaa)
+
+print("____________________________________________________________________________________________________________________________________________")
+
+allllist=[["a","b","c","d"]
+         ,["e","f","g","h"]
+         ,["i","j","k","l"]
+         ,["m","n","o","p"]]
+r=0
+c=0
+containerr=""
+while c<=3:
+    containerr=containerr+allllist[r][c]+"  "
+    c+=1
+    r+=1
+
+r=0
+c=3
+while r<=3:
+    containerr=containerr+allllist[r][c]+"  "
+    r+=1
+    c-=1
+print(containerr)
+
+print("___________________________________________________________________________________________________________________________________________")
+
+keeplist=[["a","b","c","d"]
+         ,["e","f","g","h"]
+         ,["i","j","k","l"]
+         ,["m","n","o","p"]]
+r=0
+c=0
+beakar=""
+while r<=3:
+    beakar=beakar+keeplist[r][c]+"  "
+    r+=1
+
+r=3
+c=1
+while c<=3:
+    beakar=beakar+keeplist[r][c]+"  "
+    c+=1
+
+r=2
+c=3
+while r>=0:
+    beakar=beakar+keeplist[r][c]+"  "
+    r-=1
+
+r=0
+c=2
+while c>=1:
+    beakar=beakar+keeplist[r][c]+"  "
+    c-=1
+print(beakar)
